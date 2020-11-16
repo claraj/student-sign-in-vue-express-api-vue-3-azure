c7995d4f52a21555276a3324b9dab0e7df7fe04b
clara <10088152+claraj@users.noreply.github.com>
2020-11-16 11:17:25 -0600

    tidy

--- student-client/src/main.js
+++ student-client/src/main.js
@@ -8,6 +8,7 @@ Vue.use(BootstrapVue)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.prototype.$student_api = StudentAPIService
 
 Vue.config.productionTip = false
