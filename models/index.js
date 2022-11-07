5278180eb0b711b3940fd9a410553f81b6b660f1
clara <10088152+claraj@users.noreply.github.com>
2022-11-07 14:42:22 -0600

    read db password for environment variable

--- models/index.js
+++ models/index.js
@@ -2,23 +2,22 @@ let {Sequelize, DataTypes } = require('sequelize')
 
 let env = process.env.NODE_ENV || 'development'
 
+console.log('Using environment ' + env)
+
 let config = require(__dirname + '/../config.json')[env]
+
+let password = process.env.DB_PASSWORD 
+config.password = password 
 
 let db = {}
 
-let sequelize
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config)
-} else {
-  sequelize = new Sequelize(config)
-}
+let sequelize = new Sequelize(config)
 
 let studentModel = require('./student')(sequelize, DataTypes)
 db[studentModel.name] = studentModel
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize  // sequelize object configured for our project
+db.Sequelize = Sequelize  // sequelize libary
 
 module.exports = db
 
